371f1fc1f94bb82bdf5c061a774a60b652e2ef45
lesina <lesha_lesovoy@mail.ru>
2017-09-29 12:54:09 +0300

    Reading of file 1.0

--- onegin/main.cpp
+++ onegin/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <cstdlib>
 #include <sys/stat.h>
+#include <cassert>
+#include <cmath>
+#include <unistd.h>
+#include <fcntl.h>
 
 long int getFileSize(const char *);
 
@@ -12,10 +17,17 @@ int main() {
 
     const char *filename = "test";
     FILE *file = fopen(filename, "rb");
+    int fd = open(filename, O_RDONLY);
+    assert(std::isfinite(fd));
+    assert(file);
+
     long int number_of_characters = getNumberOfCharacters(file);
     long int size_of_file = getFileSize(filename);
+    assert(std::isfinite(number_of_characters));
+    assert(std::isfinite(size_of_file));
 
-    printf("%ld", size_of_file);
+    char buf[number_of_characters];
+    read(fd, buf, size_of_file);
 
     return 0;
 }
