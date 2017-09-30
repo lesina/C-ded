37127bce2f3d563b6334aa0c294bf1fc3ae8cfa5
lesina <lesha_lesovoy@mail.ru>
2017-09-30 13:46:11 +0300

    Sorting from the begin

--- onegin/main.cpp
+++ onegin/main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
-#include <cstdlib>
 #include <sys/stat.h>
 #include <cassert>
 #include <cmath>
 #include <unistd.h>
 #include <fcntl.h>
+#include <cstring>
 
-long int getFileSize(const char *);
+long int get_file_size(const char *);
 
-long int getNumberOfCharacters(FILE *);
+long int get_number_of_characters(FILE *);
+
+size_t count_lines(const char *);
+
+void list_of_string(char **, char *);
+
+int compare(const void *, const void *);
 
 int main() {
 
@@ -21,18 +27,27 @@ int main() {
     assert(std::isfinite(fd));
     assert(file);
 
-    long int number_of_characters = getNumberOfCharacters(file);
-    long int size_of_file = getFileSize(filename);
+    long int number_of_characters = get_number_of_characters(file);
+    long int size_of_file = get_file_size(filename);
     assert(std::isfinite(number_of_characters));
     assert(std::isfinite(size_of_file));
 
     char buf[number_of_characters];
     read(fd, buf, size_of_file);
 
+    size_t number_of_lines = count_lines(buf);
+    assert(std::isfinite(number_of_lines));
+    char *text[number_of_lines];
+
+    list_of_string(text, buf);
+    qsort(text, number_of_lines, sizeof(text[0]), compare);
+
+    printf("%s", text[2]);
+
     return 0;
 }
 
-long int getFileSize(const char *filename) {
+long int get_file_size(const char *filename) {
     struct stat st;
     if (stat(filename, &st) != 0) {
         return 0;
@@ -40,7 +55,34 @@ long int getFileSize(const char *filename) {
     return st.st_size;
 }
 
-long int getNumberOfCharacters(FILE *file) {
+long int get_number_of_characters(FILE *file) {
     fseek(file, 0, SEEK_END);
     return ftell(file);
+}
+
+size_t count_lines(const char *buf) {
+    size_t number_of_lines = 1, i = 0;
+    while (buf[i]) {
+        if (buf[i] == '\n') {
+            number_of_lines++;
+        }
+        i++;
+    }
+    return number_of_lines;
+}
+
+void list_of_string(char **text, char *buf) {
+    int i = 1, line = 1;
+    text[0] = buf;
+    while (buf[i]) {
+        if (buf[i] == '\n') {
+            text[line] = buf + i + 1;
+            line++;
+        }
+        i++;
+    }
+}
+
+int compare(const void *a, const void *b) {
+    return strcmp(*(char **) a, *(char **) b);
 }
