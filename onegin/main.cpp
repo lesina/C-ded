ab986dbf26de9ee2ce985d9137f094d45394e0ce
lesina <lesha_lesovoy@mail.ru>
2017-10-04 17:41:31 +0300

    onegin UPDATE

--- onegin/main.cpp
+++ onegin/main.cpp
@@ -1,64 +1,56 @@
 #include <iostream>
 #include <sys/stat.h>
-#include <cassert>
-#include <cmath>
 #include <unistd.h>
 #include <fcntl.h>
 #include <cstring>
+
+#define FD_ERROR_OPEN(fd) if (fd == -1) { perror("open"); exit(-1); }
 
 const char *poem = "test";
 const char *dict = "dict";
 const char *rifm = "rifm";
 
-long int get_file_size(const char *);
+struct Verse {
+    char *text;
+    size_t length_of_verse = 0;
+};
 
-long int get_number_of_characters(FILE *);
+long int get_file_size(int);
 
 size_t count_lines(const char *);
 
-void list_of_string(char **, char *);
+void list_of_string(Verse [], char *);
 
 int compare(const void *, const void *);
 
 int compare_from_end(const void *, const void *);
 
-int find(char, char *);
+void write_to_file(Verse *, size_t, int);
 
-void write_to_file(char **, size_t, int);
+void sorted_writef(const char *, Verse *, size_t, int (*compare)(const void *, const void *));
 
 int main() {
 
     printf("Sorting english version of Zhenya Onegin\n"
-                   "1.1 (c) Lesha\n");
+                   "1.2 (c) Lesha\n");
 
-    FILE *file = fopen(poem, "rb");
+    errno = 0;
     int fd = open(poem, O_RDONLY);
-    assert(std::isfinite(fd));
-    assert(file);
+    FD_ERROR_OPEN(fd);
 
-    long int number_of_characters = get_number_of_characters(file);
-    long int size_of_file = get_file_size(poem);
-    assert(std::isfinite(number_of_characters));
-    assert(std::isfinite(size_of_file));
+    long int size_of_file = get_file_size(fd);
 
-    char buf[number_of_characters];
+    char buf[size_of_file];
     read(fd, buf, size_of_file);
     close(fd);
-    fclose(file);
 
     size_t number_of_lines = count_lines(buf);
-    assert(std::isfinite(number_of_lines));
-    char *text[number_of_lines];
+    Verse verse[number_of_lines];
 
-    list_of_string(text, buf);
-    qsort(text, number_of_lines, sizeof(text[0]), compare);
-    fd = open(dict, O_RDWR | O_CREAT);
-    write_to_file(text, number_of_lines, fd);
-    close(fd);
-    qsort(text, number_of_lines, sizeof(text[0]), compare_from_end);
-    fd = open(rifm, O_RDWR | O_CREAT);
-    write_to_file(text, number_of_lines, fd);
-    close(fd);
+    list_of_string(verse, buf);
+
+    sorted_writef(dict, verse, number_of_lines, compare);
+    sorted_writef(rifm, verse, number_of_lines, compare_from_end);
 
     return 0;
 }
@@ -66,30 +58,17 @@ int main() {
 //-------------------------------------------
 //! Gets size of file in bytes
 //!
-//! @param  [in]    filename    name of file with what we are working
+//! @param  [in]    fd    file descriptor
 //!
 //! @return Size of file in bytes
 //-------------------------------------------
 
-long int get_file_size(const char *filename) {
+long int get_file_size(int fd) {
     struct stat st;
-    if (stat(filename, &st) != 0) {
+    if (fstat(fd, &st) != 0) {
         return 0;
     }
     return st.st_size;
-}
-
-//-------------------------------------------
-//! Gets number of characters in file
-//!
-//! @param  [in]    file    file-pointer
-//!
-//! @return Number of characters
-//-------------------------------------------
-
-long int get_number_of_characters(FILE *file) {
-    fseek(file, 0, SEEK_END);
-    return ftell(file);
 }
 
 //-------------------------------------------
@@ -115,54 +94,59 @@ size_t count_lines(const char *buf) {
 //! Makes list of pointers to the begins of every line
 //!
 //! @param  [in]    buf     buffer with the whole text and every symbol
-//! @param  [out]   text    list of pointers to the begins of every line
+//! @param  [out]   verse   structure with the pointer to the begin of the verse
 //!
-//! @return list of pointers to the begins of every line
+//! @return list of pointers to the begins of every line and lengths of every verse
 //-------------------------------------------
 
-void list_of_string(char **text, char *buf) {
-    int i = 1, line = 1;
-    text[0] = buf;
+void list_of_string(Verse verse[], char *buf) {
+    int i = 0, line = 1;
+    size_t length = 1;
+    verse[0].text = buf;
     while (buf[i]) {
         if (buf[i] == '\n') {
-            text[line] = buf + i + 1;
+            verse[line].text = buf + i + 1;
+            verse[line - 1].length_of_verse = length - 1;
+            length = 0;
             line++;
         }
+        length++;
         i++;
     }
+    verse[line - 1].length_of_verse = length;
 }
 
 //-------------------------------------------
 //! function of comparison strings in qsort
 //!
-//! \param  [in]    a   a-string
-//! \param  [in]    b   b-string
+//! \param  [in]    a   a-verse
+//! \param  [in]    b   b-verse
 //!
 //! \return result of function srtcmp
 //-------------------------------------------
 
 int compare(const void *a, const void *b) {
-    return strcmp(*(char **) a, *(char **) b);
+    return strcmp(((Verse *) a)->text, ((Verse *) b)->text);
 }
 
 //-------------------------------------------
 //! function of comparison strings (from the end) in qsort
 //!
-//! \param  [in]    a   a-string
-//! \param  [in]    b   b-string
+//! \param  [in]    a   a-verse
+//! \param  [in]    b   b-verse
 //!
-//! \return 1 if a>b; -1 if a<b; 0 if a==b;
+//! \return >0 if a>b; <0 if a<b; 0 if a==b;
 //-------------------------------------------
 
 int compare_from_end(const void *a, const void *b) {
-    char *first_string = *(char **) a;
-    char *second_string = *(char **) b;
-    int pos_in_end_a = find('\n', first_string) - 1;
-    int pos_in_end_b = find('\n', second_string) - 1;
+    Verse *first_verse = (Verse *) a;
+    Verse *second_verse = (Verse *) b;
+    int pos_in_end_a = first_verse->length_of_verse;
+    int pos_in_end_b = second_verse->length_of_verse;
     while (pos_in_end_a && pos_in_end_b) {
-        if (first_string[pos_in_end_a] > second_string[pos_in_end_b]) {
+        if (first_verse->text[pos_in_end_a] > second_verse->text[pos_in_end_b]) {
             return 1;
-        } else if (first_string[pos_in_end_a] < second_string[pos_in_end_b]) {
+        } else if (first_verse->text[pos_in_end_a] < second_verse->text[pos_in_end_b]) {
             return -1;
         } else {
             pos_in_end_a--;
@@ -173,37 +157,36 @@ int compare_from_end(const void *a, const void *b) {
 }
 
 //-------------------------------------------
-//! Finds element in line
-//!
-//! \param  [in]    element what we are looking for
-//! \param  [in]    line    line-text where we are searching the element
-//!
-//! \return position of first meeting with the element in line
-//-------------------------------------------
-
-int find(char element, char *line) {
-    int i = 0;
-    while (line[i]) {
-        if (line[i] == element) {
-            return i;
-        }
-        i++;
-    }
-    return -1;
-}
-
-//-------------------------------------------
 //! Writes strings to file
 //!
-//! \param  [in]    text            list of string pointers to the begin of every line of peom
+//! \param  [in]    verse           list of verses with pointers to the begin of every line of peom
 //! \param  [in]    number_of_lines number of strings in text
 //! \param  [in]    fd              file descriptor where we write text
 //!
 //! \return file with text
 //-------------------------------------------
 
-void write_to_file(char **text, size_t number_of_lines, int fd) {
+void write_to_file(Verse *verse, size_t number_of_lines, int fd) {
     for (int i = 0; i < number_of_lines; i++) {
-        write(fd, text[i], find('\n', text[i]) + 1);
+        write(fd, verse[i].text, verse[i].length_of_verse + 1);
     }
+}
+
+//-------------------------------------------
+//! Sorting and writing the text, based on function compare
+//!
+//! \param  [in]    filename
+//! \param  [in]    verse
+//! \param  [in]    number_of_lines
+//! \param  [in]    cmp
+//!
+//! \return file with ordered text
+//-------------------------------------------
+
+void sorted_writef(const char *filename, Verse *verse, size_t number_of_lines, int (*cmp)(const void *, const void *)) {
+    qsort(verse, number_of_lines, sizeof(verse[0]), cmp);
+    int fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    FD_ERROR_OPEN(fd);
+    write_to_file(verse, number_of_lines, fd);
+    close(fd);
 }
