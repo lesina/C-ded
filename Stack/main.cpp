2931183717ff82e4497d28e7b580a7d1582d8901
lesina <lesha_lesovoy@mail.ru>
2017-10-06 21:39:05 +0300

    Stack: push, construct, destruct, dump, resize, stackOK added

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#define ASSERT_OK() if (!stackOK(stack)) stack_DUMP(stack)
 #define RED     "\033[1;31m"
 #define RESET   "\033[0m"
 
@@ -7,13 +8,14 @@ typedef double stackElem_t;
 struct stack_t {
     stackElem_t *data;
     int count;
+    int size;
 };
 
-void stackConstruct(stack_t *stack);
+void stackConstruct(stack_t *stack, int size);
 
 void stackDestruct(stack_t *stack);
 
-//void stackPush(stack_t *stack, stackElem_t element);
+void stackPush(stack_t *stack, stackElem_t element);
 
 //void stackPop(stack_t *stack);
 
@@ -21,17 +23,20 @@ bool stackOK(stack_t *stack);
 
 void stack_DUMP(stack_t *stack);
 
+void stackResize(stack_t *stack);
+
 int main() {
     stack_t *stack = (stack_t *) malloc(sizeof(stack_t));
-    stackConstruct(stack);
-    stack_DUMP(stack);
+    stackConstruct(stack, 10);
     stackDestruct(stack);
     return 0;
 }
 
-void stackConstruct(stack_t *stack) {
-    stack->count = 10;
-    stack->data = (stackElem_t *) malloc(stack->count * sizeof(stackElem_t));
+void stackConstruct(stack_t *stack, int size) {
+    stack->count = 0;
+    stack->size = size;
+    stack->data = (stackElem_t *) malloc(stack->size * sizeof(stackElem_t));
+    ASSERT_OK();
 }
 
 void stackDestruct(stack_t *stack) {
@@ -39,24 +44,51 @@ void stackDestruct(stack_t *stack) {
     free(stack);
 }
 
-//void stackPush(stack_t *stack, stackElem_t element)
+void stackPush(stack_t *stack, stackElem_t element) {
+    ASSERT_OK();
+    stack->data[stack->count++] = element;
+    if (stack->count == stack->size) {
+        stackResize(stack);
+    }
+    ASSERT_OK();
+}
 
 //void stackPop(stack_t *stack)
 
 bool stackOK(stack_t *stack) {
-    return stack && stack->count > 0 && stack->data;
+    return stack && stack->count >= 0 && stack->size > 0 && stack->data;
 }
 
 void stack_DUMP(stack_t *stack) {
     if (stackOK(stack)) {
-        printf("stack_t [%d] \n{\t", stack);
+        printf("stack_t [%#010x] \n{\t", stack);
+        printf("size = %d\n\t", stack->size);
+        printf("data [%d][%#010x] = {\n\t\t", stack->count, stack->data);
+        for (int i = 0; i < stack->count; i++) {
+            printf("[%d] = %lf\n\t\t", i, stack->data[i]); //TODO: MAKE IT FOR STACK_ELEM_T
+        }
+        printf("}\n");
     } else {
-        printf("stack_t [%d] (ERROR!!!)\n{\t", stack);
-    }
-    if (stack->count <= 0) {
-        printf("%s count = %d %s\n\t", RED, stack->count, RESET);
-    } else {
-        printf("count = %d\n\t", stack->count);
+        if (stack) {
+            printf("stack_t [%#010x] (ERROR!!!)\n{\t", stack);
+            if (stack->size > 0) {
+                printf("size = %d\n\t", stack->size);
+                if (stack->count >= 0){
+                    printf("data [%d][%s%#010x%s] = {}\n", stack->count, RED, stack->data, RESET);
+                } else {
+                    printf("data [%s%d%s][%#010x] = {}\n", RED, stack->count, RESET, stack->data);
+                }
+            } else {
+                printf("%ssize = %d%s\n", RED, stack->size, RESET);
+            }
+        } else {
+            printf("stack_t [%s%#010x%s] (ERROR!!!)\n{", RED, stack, RESET);
+        }
     }
     printf("}");
+}
+
+void stackResize(stack_t *stack) {
+    stack->size *= 2;
+    realloc(stack->data, stack->size * sizeof(stackElem_t));
 }
